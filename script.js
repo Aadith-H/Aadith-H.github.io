1442472a5feb65f232aa0517037462dc9f673f69
Aadith-H <131546566+Aadith-H@users.noreply.github.com>
2026-07-04 14:51:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const fadeElements = document.querySelectorAll(".fade-in");
 
+function revealOnScroll() {
+  fadeElements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight - 100) {
+      element.classList.add("visible");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
